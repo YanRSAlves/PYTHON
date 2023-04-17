0cdf2802d5b6708d7f66c75367346b0521c7f6b5
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2023-04-17 16:48:00 -0300

    att py

---
+++ Atividades e Exercícios/python 03 04 2023.py
@@ -0,0 +1,97 @@
+
+
+
+'''
+
+
+
+frutas = ['Maça', 'banana', 'pera','caju']
+frutas[1] = 'melão'
+print(frutas)
+
+frutas2 = frutas.copy()
+print(frutas)
+print(frutas2)
+
+frutas2.append('caja')
+print('Adicionando CAJA na lista Frutas 2', frutas2)
+
+
+#frutas1 = ['Maça', 'banana', 'pera','caju']
+#frutas2 = ['Abacaxi', 'melão']
+#frutas1.extend(frutas2)
+#print(frutas1)
+
+
+#frutas = ['Maça', 'banana', 'pera','caju']
+#frutas = frutas +  ['manga']
+#print(frutas)
+
+#frutas = ['maça', 'banana', 'pera', 'caju']
+
+#for x in range(len(frutas)):
+#    print(x+1,'ª fruta da lista:', frutas[x])
+
+#print()
+
+#for x in range(len(frutas)):
+#    print('{}º fruta da lista = {}'.format(x+1, frutas[x]))
+
+#print()
+#for y in frutas:
+#    print('frutas:', y)
+
+
+
+
+
+#pais =['Brasil','Chile', 'Peru', 'Portugal','Espanha', 'Alemanha', 'Holanda','HUGU', 'jaja']
+
+#if('Alemanha' in pais):
+ #   print("o pais está na lista")
+#print(len(pais[0]))
+
+
+
+ #                   0                       1
+#pais =[['Brasil','Chile', 'Peru'], ['Portugal','Espanha']]
+#           0       1       2           0           1
+
+#pais[0][1] = 'Peru'
+#print(pais)
+#pais[1][1] = 'Portugal'
+#print(pais)
+
+
+#pais = ('Acre', 'Portugal' , 'Espanha' , 'Madri' , 'Chile')
+#print()
+#print(pais[1:3])#('Portugal', 'Espanha')
+#print(pais[:3])#('Acre' ,'Portugal', 'Espanha')
+#print(pais[1:])#('Portugal' , 'Espanha','Madri', 'Chile')
+#print(pais[::2])#('Acre' , 'Espanha', 'Chile')
+#print(pais[::-1])#('Chile', 'Madri', 'Espanha', 'Portugal','Acre')
+
+
+
+
+#aluno1 = ['Gabriel', 19]
+#aluno2 = ['Guilherme', 20]
+#aluno3 = [aluno1, aluno2]
+#print(aluno1+aluno2)
+#print(aluno3)
+#print(aluno1[0],aluno1[1])
+
+
+
+
+#       0        1          2           3         4
+#pais1 = ('Acre', 'Alagoas' , 'Espanha' , 'Madri' , 'Amapa')
+#pais = ['Acre', 'Alagoas' , 'Espanha' , 'Madri' , 'Amapa']
+#pais3 = {'Acre', 'Alagoas' , 'Espanha' , 'Madri' , 'Amapa'}
+#print(type(pais1))
+#print(type(pais))
+#print(type(pais3))
+#print()
+#print(pais[3])
+#print(pais[-2])
+'''
