9a08b68c947e498c8b5c3102a81d336ba5cab577
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2024-12-13 20:33:52 -0300

    revisão

---
+++ Atividades e Exercícios/Resumo Python/Revisão.py
@@ -0,0 +1,31 @@
+# print('Hello word')
+
+# Variáveis
+
+# nome_usuario = 'Yan Rocha'  # String de 9 caracteres
+# print(nome_usuario)
+#
+# name, idade = 'Yan', 18
+# nota1 = nota2 = nota3 = 10.5
+# media = 7
+# estado = False
+#
+# #Função type()
+# print(type(nome_usuario))
+# print(type(media))
+# print(type(nota1))
+# print(type(estado))
+# print(type(type(8)))
+# print(type(1+2j))
+
+#Função isinstance()
+# a = 10
+# b = 'Sol'
+# print(isinstance(b, (int, float)))
+# def teste_type(valor):
+#     if (isinstance(valor, float)) == True:
+#         print('Valor Float')
+#     else:
+#         print('Valor Integer')
+# dado = 7.5
+# teste_type(dado)
--- Atividades e Exercícios/listasdeprova/2024.1.py
+++ Atividades e Exercícios/listasdeprova/2024.1.py
@@ -1,14 +1,14 @@
 s1 = "mano"
 s2 = "mano"
-s3 = "mano"
+s3 = "aura"
  
 def val (s1, s2, s3):
     cont =0
     if (s1 == "mano"):
-        cont= cont +1
+        cont = cont +1
     if (s2 == "mano"):
-        cont= cont +1
+        cont = cont +1
     if (s3 == "mano"):
-        cont= cont +1
+        cont = cont +1
     return cont
 print(val(s1,s2,s3))
