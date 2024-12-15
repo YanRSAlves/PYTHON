99b22cd207d96679ff25e625ae282b27fcc8f1a3
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2024-12-15 11:15:07 -0300

    Update Revisão.py

--- Atividades e Exercícios/Resumo Python/Revisão.py
+++ Atividades e Exercícios/Resumo Python/Revisão.py
@@ -29,3 +29,36 @@
 #         print('Valor Integer')
 # dado = 7.5
 # teste_type(dado)
+# OPERADOR OPERAÇÃO
+# +   SOMA
+# -   SUBTRAÇÃO
+# *   MULTIPLICAÇÃO
+# /   DIVISÃO REAL
+# //  DIVISÃO INTEIRA (SOBRA)
+# %   MODULAÇÃO(MOD)
+# **  POTÊNCIAÇÃO
+
+#ORDEM DE PRECEDÊNCIA DOS OPERADORES
+#PARÊNTESES
+#POTÊNCIAÇÃO
+#MULTIPLICAÇÃO/DIVISÃO
+#SOMA/SUBTRAÇÃO
+#ESQUERDA PARA DIREITA
+
+# idade = 15
+# altura = 1.75
+#
+# resultado = (idade >= 18) and (altura >= 1.70)
+# msg = 'Pode participar do evento? ' + str(resultado)
+#
+# print(msg)
+
+#PROGRAMA DE DISPARO DE ALARME
+
+porta = 'a'
+janela = 'f'
+
+alarme = (porta == 'a') or (janela == 'a')
+msg = 'Alarme disparado? ' + str(alarme)
+
+print(msg)
