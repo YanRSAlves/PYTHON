19aaf65ab30b29af8e85274ac7c23d861afa3afa
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2024-12-23 16:33:34 -0300

    Update Revisão.py

--- Atividades e Exercícios/Resumo Python/Revisão.py
+++ Atividades e Exercícios/Resumo Python/Revisão.py
@@ -162,4 +162,151 @@
 # print(f"QUADRADO: {area_quadrado:.3f}")
 # print(f"RETANGULO: {area_retangulo:.3f}")
 #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TROLL
+# import pyautogui
+# import time
 #
+# # Intervalo entre mensagens (em segundos)
+# interval = 0.5  # Ajuste conforme necessário
+#
+# # Mensagem a ser enviada
+# message = "sigma boy"
+#
+# # Número de mensagens
+# message_count = 100
+#
+# print("Certifique-se de abrir o navegador, acessar o chat e posicionar o cursor no campo de mensagem.")
+# print("Você tem 10 segundos para se preparar...")
+# time.sleep(10)  # Tempo para o usuário se preparar
+#
+# # Envia as mensagens
+# for i in range(message_count):
+#     pyautogui.typewrite(message)  # Digita a mensagem
+#     pyautogui.press("enter")  # Pressiona "Enter"
+#     time.sleep(interval)  # Espera antes de enviar a próxima mensagem
+#
+# print("Envio completo!")
+
+
+# lista d exercícios beecrowd dia 23 treino 2
+#
+# letra H:
+#
+#
+# x1, y1 = map(float, input().split())
+# x2, y2 = map(float, input().split())
+#
+# Distancia = (x2-x1)**2+(y2-y1)**2
+# Distancia = Distancia**0.5
+#
+# print(f'{Distancia:.4f}')
+#
+#
+#
+#
+#
+#
+# letra G:
+#
+# X = int(input())
+# Y = float(input())
+#
+# comsumo_medio = X/Y
+#
+# print(f'{comsumo_medio:.3f} km/l')
+#
+#
+#
+#
+#
+# letra F:
+#
+# a, b, c = map(int, input().split())
+#
+# maiorab = (a+b+abs(a-b))//2
+#
+# maiorabc = (maiorab+c+abs(maiorab-c))//2
+#
+# print(f'{maiorabc} eh o maior')
+#
+#
+#
+# letra E:
+#
+# A, B, C = map(float, input().split())
+#
+# pi = 3.14159
+#
+# Atriangulo = (A * C)/2
+# Acirculo = pi*(C**2)
+# Atrapezio = ((A+B)*C)/2
+# Aquadrado = B**2
+# Aretangulo = A*B
+#
+#
+# print(f'TRIANGULO: {Atriangulo:.3f}')
+# print(f'CIRCULO: {Acirculo:.3f}')
+# print(f'TRAPEZIO: {Atrapezio:.3f}')
+# print(f'QUADRADO: {Aquadrado:.3f}')
+# print(f'RETANGULO: {Aretangulo:.3f}')
+#
+#
+#
+#
+# letra D:
+#
+# raio = float(input())
+# pi = 3.14159
+#
+# VOLUME = (4.0/3.0)*pi*(raio**3)
+#
+# print(f'VOLUME = {VOLUME:.3f}')
+#
+# letra C:
+#
+# peca1, nump1, valor1 = map(float, input().split())
+# peca2, nump2, valor2 = map(float, input().split())
+#
+# total1 = nump1 * valor1
+# total2 = nump2 * valor2
+#
+# total = total1 + total2
+#
+# print(f'VALOR A PAGAR: R$ {total:.2f}')
+#
+# letra B:
+#
+#
+# name = input()
+# SALARY = float(input())
+# total_vendas = float(input())
+#
+#
+# SALARY = SALARY + (total_vendas*0.15)
+#
+# print(f'TOTAL = R$ {SALARY:.2f}')
+#
+# letra A:
+#
+# NUMBER = int(input())
+# horas = int(input())
+# valorhr = float(input())
+#
+# SALARY = horas*valorhr
+#
+# print(f'NUMBER = {NUMBER}')
+# print(f'SALARY = U$ {SALARY:.2f}')
